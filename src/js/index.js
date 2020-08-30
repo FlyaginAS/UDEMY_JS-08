158f5e0f1107bcda859cb3b31e9f8047883256f9
ruscat <rus.reg.36@gmail.com>
2020-08-30 14:04:30 +0300

    131

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,2 @@
-// Global app controller
-import x from './test';
-console.log(`this ${x} from index.js`);
+import * as search from '../models/Search';
+console.log(search.add(2, 5));
--- src/js/test.js
+++
@@ -1,2 +0,0 @@
-console.log('hellow from module');
-export default 8777777;
