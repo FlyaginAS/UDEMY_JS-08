78bc58220236a0ba6ed9c23ac120829facda7b8e
FlyaginAS <rus.reg.36@gmail.com>
2020-09-07 16:11:41 +0300

    142

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,32 @@
 import Search from './models/Search';
 
+//global state
+// search Object
+// current ricipe Object
+// shopping list Object
+// liked recipes
+const state = {};
+
+const controlSearch = async () => {
+  //1)Get query from view
+  const query = 'pizza';
+  if (query) {
+    //2) new search object and add to state
+    state.search = new Search(query);
+    //3) prepare UI for results
+
+    //4) search for recipes
+    await state.search.getResults();
+
+    //5)render results on UI
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector('.search').addEventListener('submit', (e) => {
+  e.preventDefault();
+  controlSearch();
+});
+
 const search = new Search('pizza');
-console.log(search);
 search.getResults();
